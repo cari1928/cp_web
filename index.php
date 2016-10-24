626fc3f22fec2d79e2a897f092c49a29d0d143e3
Radogan <radogan1928@hotmail.com>
2016-10-24 12:19:04 -0500

    Reestructuración - Header Publico

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-  echo "hola";
+  include('cp_web.class.php'); //acceso a sus métodos
+  $templates = $web->templateEngine(); //motor de plantillas listo
+
+  $templates->assign('titulo', 'Página Principal');
+  $templates->display('index.html');
 ?>
